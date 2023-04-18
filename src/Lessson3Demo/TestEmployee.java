5ec2c9544919d0dd1bf98735f201bd0b87954046
Jhunel Peñaflorida <jhunel.penaflorida@novare.com.hk>
2023-04-18 18:42:48 +0800

    Make the Test Employee Dynamic

--- src/Lessson3Demo/TestEmployee.java
+++ src/Lessson3Demo/TestEmployee.java
@@ -1,12 +1,28 @@
 package Lessson3Demo;
-
+import java.util.Scanner;
 public class TestEmployee {
     public static void main(String[] args) {
+        String empName;
+        int empHours;
+        double empRate;
+        double tax;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Name; ");
+        empName = sc.nextLine();
+        System.out.println("Enter your Hours Worked ");
+        empHours = sc.nextInt();
+        System.out.println("Enter your rate per hour: ");
+        empRate = sc.nextDouble();
+        System.out.println("Enter your tax ");
+        tax = sc.nextDouble();
+
         Employee emp1 = new Employee();
-        emp1.setEmployeeName("Mark");
-        emp1.setEmployeeHour(20);
-        emp1.setEmployeeRate(19.10);
-        emp1.setEmployeeTax(.10);
+        emp1.setEmployeeName(empName);
+        emp1.setEmployeeHour(empHours);
+        emp1.setEmployeeRate(empRate);
+        emp1.setEmployeeTax(tax);
+
         System.out.println(emp1.computeEmployeeSalary());
         System.out.println(emp1.computeEmployeeTax());
         System.out.println(emp1.takeHomePay());
